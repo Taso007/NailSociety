f732400c32ee691d2d3702c3ea176b4d69216092
kiu_sharangia.anastasia <sharangia.anastasia@kiu.edu.ge>
2024-10-07 18:21:01 +0400

    added .env to admin website

--- nailsocietyadmin/src/firebaseConfig.js
+++ nailsocietyadmin/src/firebaseConfig.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
 import { getStorage } from 'firebase/storage'; 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,18 +9,17 @@ import { getStorage } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD5BtE3sL6mVnRQn4rVxYktDLOevZ94oEg",
-  authDomain: "nailsociety-d0f92.firebaseapp.com",
-  projectId: "nailsociety-d0f92",
-  storageBucket: "nailsociety-d0f92.appspot.com",
-  messagingSenderId: "974640467410",
-  appId: "1:974640467410:web:10892a75c04997e68a8c0b",
-  measurementId: "G-1623QKMLWZ"
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
