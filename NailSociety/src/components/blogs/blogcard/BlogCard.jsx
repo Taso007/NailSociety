3ae8d77f79bd2614373e84b46eb237eb4c01679b
kiu_sharangia.anastasia <sharangia.anastasia@kiu.edu.ge>
2025-03-06 14:44:54 +0400

    fixed truncation

--- NailSociety/src/components/blogs/blogcard/BlogCard.jsx
+++ NailSociety/src/components/blogs/blogcard/BlogCard.jsx
@@ -6,20 +6,12 @@ import { useTranslation } from 'react-i18next';
 function BlogCard({ id, url, title, description, date }) {
   const {t} = useTranslation();
 
-  const getFirstNCharacters = (text, maxLength = 250) => {
-    if (text.length > maxLength) {
-      return text.substring(0, maxLength) + '...';
-    }
-    return text; 
-  };
-    
-
   return (
     <div className="card BlogCard" style={{ width: '19rem', height: '35rem' }}>
       <img src={url} className="card-img-top BlogImg" alt={title} />
       <div className="card-body">
         <h4 className="card-title"><strong>{title}</strong></h4>
-        <p className="card-text">{getFirstNCharacters(description)}</p>
+        <p className="card-text text-multiline-truncate"> {description}</p>
       </div>
       <div className="mb-3">
         <Link to={`/blog/${id}`} className="card-link blogcard-link">{t('read_more')}</Link>
